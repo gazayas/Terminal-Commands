f51b5e1bb03dbc05b183e729515fac56bb6d5fab
Gabriel Zayas <g-zayas@hotmail.com>
2022-03-21 12:18:29 +0900

    Add commands to open links, files and folders in linux and macosx

--- lib/terminal_commands.rb
+++ lib/terminal_commands.rb
@@ -3,6 +3,45 @@
 require_relative "terminal_commands/version"
 
 module TerminalCommands
+  def self.open_file_or_link(file_or_link, options = {})
+    command = if macosx?
+      "open"
+    elsif linux?
+      "xdg-open"
+    end
+    `#{command} #{file_or_link}`
+  end
+
+  def self.open_folder(path_name, options = {})
+    command = if macosx?
+      "open"
+    elsif linux?
+      "nautilus"
+    end
+    `#{command} #{path_name}`
+  end
+
+  private
+
+  def self.os
+    Gem::Platform.local.os
+  end
+
+  def self.macosx?
+    os == macosx
+  end
+
+  def self.linux?
+    os == linux
+  end
+
+  def self.macosx
+    "darwin"
+  end
+
+  def self.linux
+    "linux"
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
 end
--- test/terminal_commands_test.rb
+++ test/terminal_commands_test.rb
@@ -6,8 +6,4 @@ class TerminalCommandsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TerminalCommands::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
